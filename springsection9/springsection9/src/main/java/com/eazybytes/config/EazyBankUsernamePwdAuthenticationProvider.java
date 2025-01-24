cd1a6fafcb32818ea441304271a794773ef1d2d3
bassem <bassemm6@gmail.com>
2025-01-24 18:33:36 +0200

    section 9

---
+++ springsection9/springsection9/src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,46 @@
+package com.eazybytes.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!prod")
+@RequiredArgsConstructor
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final EazyBankUserDetailsService eazyBankUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * @param authentication the authentication request object.
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = eazyBankUserDetailsService.loadUserByUsername(username);
+        //no checks for lower env (such as dev )
+        return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+
+
+    }
+
+    /**
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
---
+++ springsection9/springsection9/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -0,0 +1,112 @@
+package com.eazybytes.config;
+
+import com.eazybytes.exceptionhandling.CustomAccessDeniedHandler;
+import com.eazybytes.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import com.eazybytes.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("prod")
+public class ProjectSecurityProdConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
+        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
+        http.securityContext(config -> config.requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+
+                    /**
+                     * @param request
+                     * @return
+                     */
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers("/notices", "/contact", "/error", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .requiresChannel(var -> var.anyRequest().requiresSecure()) // https only accepted
+                .authorizeHttpRequests((requests) -> requests
+                        /*.requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
+               .requestMatchers("/myBalance").hasAnyAuthority("VIEWBALANCE", "VIEWACCOUNT")
+               .requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
+               .requestMatchers("/myCards").hasAuthority("VIEWCARDS")*/
+                        .requestMatchers("/myAccount").hasRole("USER")
+                        .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myCards").hasRole("USER")
+                        .requestMatchers("/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(var -> var.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(var -> var.accessDeniedHandler(new CustomAccessDeniedHandler()));
+        return http.build();
+    }
+
+
+    /*
+     * Provides a simple {@link UserDetailsService} that stores a user named "user" with the password "password"
+     * and the role "USER". This is sufficient for testing the basic security features.
+     *
+     * @return a {@link UserDetailsService} that provides a single user.
+     */
+
+
+    /*
+     * Provides a default {@link PasswordEncoder} bean.
+     * This encoder uses a {@link DelegatingPasswordEncoder} that delegates encoding
+     * to other encoders based on a prefix in the password.
+     * This ensures compatibility with different password storage formats.
+     *
+     * @return a {@link PasswordEncoder} instance.
+     */
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * Provides a {@link CompromisedPasswordChecker} that delegates to the HaveIBeenPwned API to check if the
+     * password has been compromised before.
+     *
+     * @return a {@link CompromisedPasswordChecker} that checks if the password has been compromised.
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
