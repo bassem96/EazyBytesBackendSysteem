8fd991608e70bed2d0e616afde1187f8bf5509dd
bassem <bassemm6@gmail.com>
2025-01-24 19:23:29 +0200

    section 10

---
+++ springsection10/springsection10/src/main/java/com/eazybytes/config/EazyBankProdUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,49 @@
+package com.eazybytes.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("prod")
+@RequiredArgsConstructor
+public class EazyBankProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final EazyBankUserDetailsService eazyBankUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    /**
+     * @param authentication the authentication request object.
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = eazyBankUserDetailsService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username,password,userDetails.getAuthorities());
+        }else{
+            throw new BadCredentialsException("Invalid password");
+        }
+
+    }
+
+    /**
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
---
+++ springsection10/springsection10/src/main/java/com/eazybytes/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.eazybytes.exceptionhandling;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    /**
+     * @param request       that resulted in an <code>AuthenticationException</code>
+     * @param response      so that the user agent can begin authentication
+     * @param authException that caused the invocation
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        // Populate dynamic values
+        LocalDateTime currentTimeStamp = LocalDateTime.now();
+        String message = (authException != null && authException.getMessage() != null) ? authException.getMessage()
+                : "Unauthorized";
+        String path = request.getRequestURI();
+        response.setHeader("EazyBank-error-reason", "authentication failed");
+        // response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        String jsonResponse =
+                String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}",
+                        currentTimeStamp, HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                        message, path);
+        response.getWriter().write(jsonResponse);
+    }
+}
